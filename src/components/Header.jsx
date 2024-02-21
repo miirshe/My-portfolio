20ff65fda2d59b39c85a8ae0f43b8a6deb835b75
miirshe <miirshe@gmail.com>
2024-02-21 18:32:10 +0300

    Created: My Portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Header = () => {
+  return (
+    <div className='w-full'>
+        <h1 className='text-xl tracking-widest font-medium'>Miirshe</h1>
+    </div>
+  )
+}
+
+export default Header
