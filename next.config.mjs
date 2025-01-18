08f1f10bc110c9ba51c4df6a5e5e085fb23628e0
Abdikafi Isse Isak <miirshe@gmail.com>
2025-01-18 22:49:05 +0300

    refactor : next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', 
+  distDir: 'dist',
+  images: {
+    domains: ['example.com'], 
+  },
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
