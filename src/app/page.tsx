629d0d56123785a0e11ac1ae0ef0f0ca0320a32a
Abdikafi Isse Isak <miirshe@gmail.com>
2025-01-18 22:46:28 +0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
 import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
 import { DATA } from "@/data/resume";
 import Markdown from "react-markdown";
 import ContactForm from "@/components/contact-form";
